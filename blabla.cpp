fe2f89c327d854318b9dba23692c82cd1bcc8db2
vatuantrinh <trinhvantuan1103@gmail.com>
2024-07-21 18:37:47 +0700

    fix blabla

--- blabla.cpp
+++ blabla.cpp
@@ -1,8 +1,17 @@
+#include <stdio.h>
+int main(){
+    int n;
+    printf( "\n nhap n:");
+    scanf("%d",&n);
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n-i;j++){
+            printf("~");
+        }int cnt=2;
+        for( int j=1;j<=2*i-1;j++){
+            if(j<i) printf("%d",cnt), cnt+=2;
+            else printf("%d",cnt), cnt-=2;
 
-#include <iostream>
-using namespace std;
-
-int main() {
-  cout << "Hello World!mm";
-  return 0;
+        }
+        printf("\n");
+    }
 }
